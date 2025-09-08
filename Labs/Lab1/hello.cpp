f96284ce0a89a10ef5ade1809cc9e754c2e8f102
bashyahub <Sebastian.Kmiotek1@marist.edu>
2025-09-08 14:27:06 -0500

    finishing lab 1
    successfully completed lab 1

--- Labs/Lab1/hello.cpp
+++ Labs/Lab1/hello.cpp
@@ -1,7 +1,7 @@
 #ifndef CPP_HELLO
 #define CPP_HELLO
 #include <iostream>
-#include <SFLM/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 using namespace sf;
@@ -13,18 +13,41 @@ int main()
 	RenderWindow window({ 500, 250 }, "Lab 1", Style::Default);
 
 	Font font;
-	if (!font.loadFromFile("../../SFML-2.6.0/examples/opengl/resources/tuffy.ttf"))
+	if (!font.loadFromFile("../../SFML-2.6.2/examples/opengl/resources/tuffy.ttf"))
 		return 1;
 
 	Text text("Hello World!", font, 75);
 	text.setPosition(75, 75);
 
-	while (true)
-	{
+	const int START_SECONDS = 10;
+	Clock clock;
+
+	bool showZeroThisFrame = false;
+
+	while (window.isOpen()) {
+		Event e;
+		while (window.pollEvent(e)) {
+			if (e.type == Event::Closed) window.close();
+		}
+
+		int elapsed = static_cast<int>(clock.getElapsedTime().asSeconds());
+		int remaining = START_SECONDS - elapsed;
+		if (remaining < 0) remaining = 0;
+
+		text.setString(to_string(remaining));
+
 		window.clear();
 		window.draw(text);
 		window.display();
 
+		if (remaining == 0) {
+			if (showZeroThisFrame) {
+				window.close();
+			}
+			else {
+				showZeroThisFrame = true;
+			}
+		}
 	}
 	return 0;
 }
