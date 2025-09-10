064c6f0e0a4c4e04c21c2b622f64f75a6c1b0402
bashyahub <Sebastian.Kmiotek1@marist.edu>
2025-09-10 13:03:31 -0500

    Creating game window and sprites
    this commit creates the game window for the game to be played in, as well as setting up sprites like the tree sprite

--- Projects/Game1/code/Timber.cpp
+++ Projects/Game1/code/Timber.cpp
@@ -24,6 +24,34 @@ int main()
 	isAlive = 3.141f;
 
 
+	// Create a video mode object
+	VideoMode vm(1920, 1080);
+
+	// Create and open game window
+	RenderWindow window(vm, "Timber!", Style::Default);	
+
+	// Create a texture to hold a graphic on the GPU
+	Texture textureBackground;
+
+	// Load a graphic into the texture from a file
+	textureBackground.loadFromFile("graphics/background.png");
+
+	// Create a sprite
+	Sprite spriteBackground;
+
+	// Attaching a texture to the sprite
+	spriteBackground.setTexture(textureBackground);
+
+	//Setting spriteBackground to cover screen
+	spriteBackground.setPosition(0, 0);
+
+	//Creating tree sprite
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png")
+		Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+
 
 }
 
