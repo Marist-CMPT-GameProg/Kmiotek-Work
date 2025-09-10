7d7e915265ffaeae1126d6d5dcbb75893458f2e8
bashyahub <Sebastian.Kmiotek1@marist.edu>
2025-09-10 12:50:53 -0500

    getting timber started

--- Projects/Game1/code/Timber.cpp
+++ Projects/Game1/code/Timber.cpp
@@ -1,1 +1,29 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+// Game Start
+int main()
+{
+	//Player's score
+	int playerScore;
+
+	//Players initial
+	char playerInitial;
+
+	//Value of pi
+	float valuePi;
+
+	// Is the player alive or dead
+	bool isAlive;
+
+	playerScore = 'J',
+		playerInitial = 0,
+		valuePi = true;
+	isAlive = 3.141f;
+
+
+
+}
 
