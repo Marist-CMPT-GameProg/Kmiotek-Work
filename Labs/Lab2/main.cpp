ff1a93ce34f40a09f267b5d568bdf8baf7495845
bashyahub <Sebastian.Kmiotek1@marist.edu>
2025-09-23 16:30:44 -0400

    update lab2
    added bee sprite and make it rotate around the sunflower

--- Labs/Lab2/main.cpp
+++ Labs/Lab2/main.cpp
@@ -1,11 +1,28 @@
+// Environment configuration
+// PATH=..\..\SFML-2.6.2\bin;$(PATH)
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
 using namespace std;
 using namespace sf;
 
+//Global bee orbit variables
+Vector2f orbitCenter(1024.f / 2.f, 1024.f / 2.f);
+float orbitRadius = 150.0f;
+float orbitSpeed = 1.5f;
+float orbitAngle = 0.f;
+
+void updateBee(Sprite& bee, float dt) {
+	orbitAngle += orbitSpeed * dt;
+
+	float x = orbitCenter.x + orbitRadius * cos(orbitAngle);
+	float y = orbitCenter.y + orbitRadius * sin(orbitAngle);
+	bee.setPosition(x, y);
+}
 int main() {
 	RenderWindow window(VideoMode(500, 500), "Lab2 Window");
+
+	Clock clock;
 
 	Texture textureGrass;
 	textureGrass.loadFromFile("resources/grass.png");
@@ -15,10 +32,16 @@ int main() {
 	textureSunflower.loadFromFile("resources/sunflower.png");
 	Sprite sunflower(textureSunflower);
 
+	Texture textureBee;
+	textureBee.loadFromFile("resources/bee.png");
+	Sprite bee(textureBee);
+
 	sunflower.setPosition(
 		1024.f / 2.f - sunflower.getGlobalBounds().width / 2.f,
 		1024.f / 2.f - sunflower.getGlobalBounds().height / 2.f
 	);
+
+	updateBee(bee, 0.f);
 
 	View view(FloatRect(0.f, 0.f, 1024.f, 1024.f));
 	window.setView(view);
@@ -29,14 +52,20 @@ int main() {
 			if (event.type == Event::Closed)
 				window.close();
 
-			if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
+			if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) 
 				window.close();
-			}
 		}
+
+		float dt = clock.restart().asSeconds();
+
+		updateBee(bee, dt);
+		
 		window.clear();
 		window.draw(grass);
 		window.draw(sunflower);
+		window.draw(bee);
 		window.display();
 	}
 	return 0;
 }
+
