e2cc1d4814bc9893dc978d89285131cad6eef715
bashyahub <Sebastian.Kmiotek1@marist.edu>
2025-10-02 23:37:27 -0500

    Update Bat.cpp
    set various bat characteristics such as its bounds, movement, position, color etc. Set bat to be updated using delta time.

--- Projects/Pong/Bat.cpp
+++ Projects/Pong/Bat.cpp
@@ -1,60 +1,55 @@
 #include "Bat.h" 
 
+using namespace std;
+
 //Constructor to initialize the bat
-Bat::Bat(float startX, float startY) : m_Position(startX, startY)
+Bat::Bat(float startX, float startY) : m_Position(startX, startY) , m_FixedY(startY)
 {
 	//Set bat size
-	m_Shape.setSize(Vector2f(50, 5));
-	//Set bat color
-	m_Shape.setFillColor(Color::Red);
-	//Set bat position
+	m_Shape.setSize({ 110.f, 24.f });
+	//Set its position
 	m_Shape.setPosition(m_Position);
+	//Set its color
+	m_Shape.setFillColor(Color(220, 180, 80)); 
+	//Set thickness of bat outline and its color
+	m_Shape.setOutlineThickness(2.f);
+	m_Shape.setOutlineColor(Color::Black);
+
+	m_MinX = 0.f;
+	m_MaxX = 99999.f;
+
+	m_HasSprite = false;
+	syncSprite();
 }
 
-FloatRect Bat::getPosition()
-{
-	return m_Shape.getGlobalBounds();
+//Set bounds for bat movement 
+void Bat::setXBounds(float minX, float maxX) {
+	m_MinX = min(minX, maxX);
+	m_MaxX = max(minX, maxX);
 }
-RectangleShape Bat::getShape()
-{
-	return m_Shape;
-}
-void Bat::moveLeft()
-{
-	m_MovingLeft = true;
-}
-void Bat::moveRight()
-{
-	m_MovingRight = true;
-}
-void Bat::stopLeft()
-{
-	m_MovingLeft = false;
-}
-void Bat::stopRight()
-{
-	m_MovingRight = false;
-}
-void Bat::update(Time dt)
-{
-	//Update bat position
-	if (m_MovingLeft)
-	{
-		m_Position.x -= m_Speed * dt.asSeconds();
+
+void Bat::moveLeft() {m_MovingLeft = true;}
+void Bat::moveRight() {m_MovingRight = true;}
+void Bat::stopLeft() {m_MovingLeft = false;}
+void Bat::stopRight() {m_MovingRight = false;}
+
+//Update the bat using delta time
+void Bat::update(Time dt) {
+	const float dtSeconds = dt.asSeconds();
+
+	if (m_MovingLeft) {
+		m_Position.x -= m_Speed * dtSeconds;
 	}
-	if (m_MovingRight)
-	{
-		m_Position.x += m_Speed * dt.asSeconds();
+	if (m_MovingRight) {
+		m_Position.x += m_Speed * dtSeconds;
 	}
-	//Make sure the bat doesn't leave the screen
-	if (m_Position.x < 0)
-	{
-		m_Position.x = 0;
-	}
-	if (m_Position.x > 800 - m_Shape.getSize().x)
-	{
-		m_Position.x = 800 - m_Shape.getSize().x;
-	}
-	//Update the bat's position
+	//Keep the bat within horizontal bounds
+	m_Position.y = m_FixedY;
+
+	const float maxAllowed = m_MaxX - m_Shape.getSize().x;
+	if (m_Position.x < m_MinX) m_Position.x = m_MinX;
+	if (m_Position.x > maxAllowed) m_Position.x = maxAllowed;
 	m_Shape.setPosition(m_Position);
+	syncSprite();
 }
+
